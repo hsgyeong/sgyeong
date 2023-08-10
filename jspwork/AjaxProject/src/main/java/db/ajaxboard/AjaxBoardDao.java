a7aed4c0ed673e0438d4ed6553af83b93914657a
hsgyeong <sgyeong0711@gmail.com>
2023-08-10 17:04:40 +0900

    0810

--- jspwork/AjaxProject/src/main/java/db/ajaxboard/AjaxBoardDao.java
+++ jspwork/AjaxProject/src/main/java/db/ajaxboard/AjaxBoardDao.java
@@ -73,7 +73,7 @@ public class AjaxBoardDao {
 		return list;
 		
 	}
-	
+
 	//한개 데이터 반환
 	
 	public AjaxBoardDto getData(String num)
@@ -109,6 +109,30 @@ public class AjaxBoardDao {
 			db.dbClose(rs, pstmt, conn);
 		}return dto;	
 		
+	}
+	
+	public void updateBoard(AjaxBoardDto dto)
+	{
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		
+		String sql="update ajaxboard set writer=?, subject=?, story=?, avata=? where num=?";
+		
+		try{
+			pstmt=conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getSubject());
+			pstmt.setString(3, dto.getStory());
+			pstmt.setString(4, dto.getAvata());
+			pstmt.setString(5, dto.getNum());
+			
+			pstmt.execute();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
 	}
 	
 	public void deleteBoard(String num)
